66d37f40dcf3d29878b520b409e6196fe43e151d
Causelove_NA <kozlov.na@phystech.edu>
2023-05-06 15:24:15 +0300

    now it slightly works

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <array>
-#include <hdf5/serial/hdf5.h>
+#include "hdf5/serial/hdf5.h"
+// #include "hdf5/serial/H5Cpp.h"
 #include <fstream>
 #include <string>
 
@@ -14,8 +15,8 @@ using json = nlohmann::json;
 using namespace std;
 
 template<const int x_size, const int y_size>
-array<array<double, y_size + 2>, x_size + 2> step(array<array<double, y_size + 2>, x_size + 2> phi, double dt, double dx, double dy,
- const double phi_0, const double phi_1)
+array<array<double, y_size + 2>, x_size + 2> step(array<array<double, y_size + 2>, x_size + 2>& phi, double& dt, double& dx, double& dy,
+ const double& phi_0, const double& phi_1)
 //operator that counts field of phi configuration on next step in time
 {
     array<array<double, y_size + 2>, x_size + 2> phi_new{0}; //new phi field (initialized with zeros)
@@ -39,15 +40,31 @@ array<array<double, y_size + 2>, x_size + 2> step(array<array<double, y_size + 2
     return phi_new;
 }
 
+template<const int x_size, const int y_size>
+void write_to_file(array<array<double, y_size + 2>, x_size + 2>& phi, string OUTPATH)
+{
+    ofstream out;
+        out.open(OUTPATH);
+        if (out.is_open())
+        {
+            for (int j = 1; j < y_size; j++){
+                for (int i = 1; i < x_size; i++)
+                out <<  phi[i][j] <<'\n';
+            }
+        }
+        out.close();
+} 
+
 int main(int argc, char* argv[])
 {    
-    const double phi_0 = 1;
+    const double phi_0 = 2;
     const double phi_1 = 0;
-    const int x_size = 10; //horizontal size of grid
-    const int y_size = 10;//vertical size of grid
-    double dt = 0.01;
-    double dx = 0.001;
-    double dy = 0.001;
+    const int x_size = 30; //horizontal size of grid
+    const int y_size = 30;//vertical size of grid
+    double dt = 0.1;
+    double dx = 1;
+    double dy = 1;
+    double num_of_iter = 100000;
     array<array<double, y_size + 2>, x_size + 2> phi{0}; //+2 stands for buffer boundaries
     //initiate boundary conditions in buffers
     for (int j = 0; j < y_size; j++) 
@@ -60,6 +77,13 @@ int main(int argc, char* argv[])
         phi[i][1] = phi[i][0];              //horizontal bounds
         phi[i][y_size] = phi[i][y_size+1];
     }
-    phi = step<x_size, y_size>(phi, dt, dx, dy, phi_0, phi_1);
+    //count grid configuration
+    for (int i = 0; i < num_of_iter; i++)
+    {
+        phi = step<x_size, y_size>(phi, dt, dx, dy, phi_0, phi_1);
+    }
+
+    write_to_file<x_size, y_size>(phi, "grid150000steps.txt");
+
     printf("\nover\n");
 }
