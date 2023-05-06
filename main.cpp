00bff0b95fbeeae1d1b3ffe06a7868669cc10ebb
Causelove_NA <kozlov.na@phystech.edu>
2023-05-06 16:15:33 +0300

    fixed loops n+2

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 // #include "hdf5/serial/H5Cpp.h"
 #include <fstream>
 #include <string>
+#include <omp.h>
 
 using namespace std;
 
@@ -20,22 +21,22 @@ array<array<double, y_size + 2>, x_size + 2> step(array<array<double, y_size + 2
 //operator that counts field of phi configuration on next step in time
 {
     array<array<double, y_size + 2>, x_size + 2> phi_new{0}; //new phi field (initialized with zeros)
-    for (int i = 0; i < x_size; i++)
+    for (int i = 0; i < x_size+2; i++)
     {
-        for (int j = 0; j < y_size; j++)
+        for (int j = 0; j < y_size+2; j++)
         {
             phi_new[i][j] = phi[i][j] + dt * ((phi[i+1][j] - 2*phi[i][j] + phi[i-1][j])/(dx*dx) + (phi[i][j+1] - 2*phi[i][j] + phi[i][j-1])/(dy*dy));
         }
     }
-    for (int j = 0; j < y_size; j++) 
+    for (int j = 0; j < y_size+2; j++) 
     {
         phi_new[0][j] = 2*phi_0 - phi[1][j];                    //vertical bounds
-        phi_new[x_size+1][j] = - 2*phi_1 + phi[x_size][j];
+        phi_new[x_size+1][j] = 2*phi_1 - phi[x_size][j];
     } 
-    for (int i = 0; i < x_size; i++)
+    for (int i = 0; i < x_size+2; i++)
     {
-        phi_new[i][1] = phi_new[i][0];              //horizontal bounds
-        phi_new[i][y_size] = phi_new[i][y_size+1];
+        phi_new[i][0] = phi_new[i][1];              //horizontal bounds
+        phi_new[i][y_size+1] = phi_new[i][y_size];
     }
     return phi_new;
 }
@@ -67,15 +68,15 @@ int main(int argc, char* argv[])
     double num_of_iter = 100000;
     array<array<double, y_size + 2>, x_size + 2> phi{0}; //+2 stands for buffer boundaries
     //initiate boundary conditions in buffers
-    for (int j = 0; j < y_size; j++) 
+    for (int j = 0; j < y_size+2; j++) 
     {
         phi[0][j] = 2*phi_0 - phi[1][j];                    //vertical bounds
         phi[x_size+1][j] = - 2*phi_1 + phi[x_size][j];
     } 
-    for (int i = 0; i < x_size; i++)
+    for (int i = 0; i < x_size+2; i++)
     {
-        phi[i][1] = phi[i][0];              //horizontal bounds
-        phi[i][y_size] = phi[i][y_size+1];
+        phi[i][0] = phi[i][1];              //horizontal bounds
+        phi[i][y_size+1] = phi[i][y_size];
     }
     //count grid configuration
     for (int i = 0; i < num_of_iter; i++)
