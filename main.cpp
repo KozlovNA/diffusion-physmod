cd9d0cc251ac27a5727b71615ab3dba71a4ccda6
Causelove_NA <kozlov.na@phystech.edu>
2023-05-06 10:12:27 +0300

    added boundary handling in operator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <array>
 #include <hdf5/serial/hdf5.h>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
@@ -27,19 +29,25 @@ array<array<double, y_size + 2>, x_size + 2> step(array<array<double, y_size + 2
     for (int j = 0; j < y_size; j++)
     {
         phi_new[0][j] = 2*phi_0 - phi[1][j];
+        phi_new[x_size+1][j] = - 2*phi_1 + phi[x_size][j];
     } 
+    for (int i = 0; i < x_size; i++)
+    {
+        phi_new[i][1] = phi_new[i][0];
+        phi_new[i][y_size] = phi_new[i][y_size+1];
+    }
     return phi_new;
 }
 
 int main(int argc, char* argv[])
-{
-    //initializing grid
-    const int x_size = 10; //horizontal size of grid
-    const int y_size = 10; //vertical size of grid
+{    
     const double phi_0 = 1;
     const double phi_1 = 0;
+    const int x_size = 10; //horizontal size of grid
+    const int y_size = 10;//vertical size of grid
     array<array<double, y_size + 2>, x_size + 2> phi{0}; //+2 stands for buffer boundaries
     //initiate boundary conditions in buffers
     //<...>
     //
+    printf("over");
 }
